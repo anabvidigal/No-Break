4dd3e1baddcc72b3bbbd2f2ee911630be761b941
ana <anabvidigal@gmail.com>
2022-02-02 18:15:59 -0300

    Animation for intro

---
+++ Bike-Runner/AnimationViewController.swift
@@ -0,0 +1,59 @@
+//
+//  AnimationViewController.swift
+//  Bike-Runner
+//
+//  Created by Ana Bittencourt Vidigal on 02/02/22.
+//
+
+
+import UIKit
+import SpriteKit
+import GameplayKit
+
+class AnimationViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+        // added landscape orientation
+        let value = UIInterfaceOrientation.landscapeLeft.rawValue
+        UIDevice.current.setValue(value, forKey: "orientation")
+        
+        if let view = self.view as! SKView? {
+            // Load the SKScene from 'GameScene.sks'
+            if let scene = SKScene(fileNamed: "AnimationScene") {
+                // Set the scale mode to scale to fit the window
+                scene.scaleMode = .aspectFit
+                
+                // Present the scene
+                view.presentScene(scene)
+            }
+            
+            view.ignoresSiblingOrder = true
+            
+            view.showsFPS = true
+            view.showsNodeCount = true
+        }
+    }
+
+    override var shouldAutorotate: Bool {
+        return true
+    }
+
+    override var supportedInterfaceOrientations: UIInterfaceOrientationMask {
+        
+        // added return landscape
+        return .landscapeLeft
+        
+        // commented out the original content
+//        if UIDevice.current.userInterfaceIdiom == .phone {
+//            return .allButUpsideDown
+//        } else {
+//            return .all
+//        }
+    }
+
+    override var prefersStatusBarHidden: Bool {
+        return true
+    }
+}
